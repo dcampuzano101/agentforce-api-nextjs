0b00a4b0aac4799faabe4207a11d97197d660a96
David Campuzano <dcampuzano101@gmail.com>
2025-03-07 13:53:06 -0500

    adds framer motion and changes SF_Domain env variable in dashboard.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,39 @@
+"use client";
+
+import { useEffect } from "react";
 import Dashboard from "@/components/Dashboard";
 import Image from "next/image";
 import agentforceRobot from "@/lib/images/agentforce-robot.png";
+import { motion } from "framer-motion";
+
+console.log("Framer motion imported:", motion);
 
 export default function Home() {
+
   return (
     <div className="relative">
       <main className="flex w-full">
-        <div className="fixed left-10 top-1/2 -translate-y-1/2">
+        <motion.div
+          className="fixed left-10 top-1/3 -translate-y-1/2"
+          animate={{
+            y: [-10, 10, -10],
+          }}
+          transition={{
+            duration: 3,
+            ease: "easeInOut",
+            repeat: Infinity,
+          }}
+          whileHover={{ scale: 1.05 }}
+        >
           <Image
             src={agentforceRobot}
             alt="Agentforce Robot"
             width={450}
             height={450}
             className="object-contain"
+            priority
           />
-        </div>
+        </motion.div>
         <div className="w-[2000px]">
           <Dashboard />
         </div>
