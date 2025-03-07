0ea56d3bc92e997a567c24b20e1f4f9250e1015c
David Campuzano <dcampuzano101@gmail.com>
2025-03-07 15:19:04 -0500

    adds another agent image floating

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-import { useEffect } from "react";
 import Dashboard from "@/components/Dashboard";
 import Image from "next/image";
 import agentforceRobot from "@/lib/images/agentforce-robot.png";
+import agentforceStanding from "@/lib/images/agentforce-standing.png";
 import { motion } from "framer-motion";
 
 console.log("Framer motion imported:", motion);
 
 export default function Home() {
-
   return (
     <div className="relative">
       <main className="flex w-full">
         <motion.div
-          className="fixed left-10 top-1/3 -translate-y-1/2"
+          className="fixed left-10 top-1/2 -translate-y-1/2 mt-24"
           animate={{
             y: [-10, 10, -10],
           }}
@@ -26,11 +25,33 @@ export default function Home() {
           whileHover={{ scale: 1.05 }}
         >
           <Image
+            src={agentforceStanding}
+            alt="agentforce standing"
+            width={250}
+            height={250}
+            className="object-contain scale-x-[-1]"
+            priority
+          />
+        </motion.div>
+        <motion.div
+          className="fixed right-10 top-1/4 -translate-y-1/2"
+          animate={{
+            y: [-10, 10, -10],
+          }}
+          transition={{
+            duration: 3,
+            ease: "easeInOut",
+            repeat: Infinity,
+            delay: 1.5,
+          }}
+          whileHover={{ scale: 1.05 }}
+        >
+          <Image
             src={agentforceRobot}
             alt="Agentforce Robot"
             width={450}
             height={450}
-            className="object-contain"
+            className="object-contain "
             priority
           />
         </motion.div>
